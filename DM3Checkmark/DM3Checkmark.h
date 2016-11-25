77e04f1b2297ddc40fd0909566a4bb19f429d2af
Sergey Simonov <ssimonov@me.com>
2016-11-25 12:59:47 +0300

    Added dynamic framework

---
+++ DM3Checkmark/DM3Checkmark.h
@@ -0,0 +1,20 @@
+//
+//  DM3Checkmark.h
+//  DM3Checkmark
+//
+//  Created by Сергей Симонов on 25/11/16.
+//  Copyright © 2016 Dmitry Beloborodov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <DM3Checkmark/DM3CheckmarkView.h>
+
+//! Project version number for DM3Checkmark.
+FOUNDATION_EXPORT double DM3CheckmarkVersionNumber;
+
+//! Project version string for DM3Checkmark.
+FOUNDATION_EXPORT const unsigned char DM3CheckmarkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DM3Checkmark/PublicHeader.h>
+
+
